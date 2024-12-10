264de9d4ed2c6a3756b4808b162ed7976b00ca4c
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2024-12-10 17:12:21 +0300

    Update items.py

--- scrapy_project/items.py
+++ scrapy_project/items.py
@@ -6,12 +6,24 @@
 # Модуль Items.py Нужен для упаковки данных
 import scrapy
 
+# обработчики для лоадеров (каждый обработчик или ПРЕобработчик или ПОСТобработчик)
+from itemloaders.processors import TakeFirst, MapCompose, Compose
+
+
+def correct_text(value):
+    value = value.strip()
+    return value
+
 class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
-    commit = scrapy.Field() # Обьявляю поля которые надо вытянуть из Паука (смотри hhru, эти переменные есть там)
-    author = scrapy.Field()
-    loggin_of_author = scrapy.Field()
-    Published_datatime = scrapy.Field()
-    url_img = scrapy.Field()
+    # output_processor=TakeFirst()
+    commit = scrapy.Field(input_processor=MapCompose(correct_text)) # Обьявляю поля которые надо вытянуть из Паука (смотри hhru, эти переменные есть там)
+    author = scrapy.Field(output_processor=TakeFirst())
+    loggin_of_author = scrapy.Field(output_processor=TakeFirst())
+    Published_datatime = scrapy.Field(output_processor=TakeFirst())
+    url_img = scrapy.Field(output_processor=TakeFirst())
+    print(commit)
+    print()
+
 
 
