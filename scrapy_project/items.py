bd8ae05ba917a3b16e98ba5e909885a1750da44f
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2024-12-10 20:55:46 +0300

    Update items.py

--- scrapy_project/items.py
+++ scrapy_project/items.py
@@ -10,20 +10,26 @@ import scrapy
 from itemloaders.processors import TakeFirst, MapCompose, Compose
 
 
-def correct_text(value):
+def correct_text_strip(value):
     value = value.strip()
     return value
+
+def correct_text_del_word(value):
+    value = value[1:]
+    return value
+
+def correct_url(value:str):
+    return value.split(" ")[0]
 
 class JobparserItem(scrapy.Item):
     # define the fields for your item here like:
     # output_processor=TakeFirst()
-    commit = scrapy.Field(input_processor=MapCompose(correct_text)) # Обьявляю поля которые надо вытянуть из Паука (смотри hhru, эти переменные есть там)
-    author = scrapy.Field(output_processor=TakeFirst())
-    loggin_of_author = scrapy.Field(output_processor=TakeFirst())
+    commit = scrapy.Field(input_processor=MapCompose(correct_text_strip)) # Обьявляю поля которые надо вытянуть из Паука (смотри hhru, эти переменные есть там)
+    author = scrapy.Field()
+    loggin_of_author = scrapy.Field(input_processor=MapCompose(correct_text_del_word), output_processor=TakeFirst())
     Published_datatime = scrapy.Field(output_processor=TakeFirst())
-    url_img = scrapy.Field(output_processor=TakeFirst())
-    print(commit)
-    print()
+    url_img = scrapy.Field(input_processor=MapCompose(correct_url), output_processor=TakeFirst())
+
 
 
 
