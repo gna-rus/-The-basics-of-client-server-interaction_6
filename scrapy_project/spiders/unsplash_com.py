051aec8c57a9c2946e6f28f46f10b9a7652aa4ac
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2024-12-13 15:09:48 +0300

    Update unsplash_com.py

--- scrapy_project/spiders/unsplash_com.py
+++ scrapy_project/spiders/unsplash_com.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy.http import HtmlResponse # Импортирую методы типа HtmlResponse
 from pprint import pprint
 from scrapy import Selector
-from scrapy_project.items import JobparserItem
+from jobparser.items import JobparserItem
 from scrapy.loader import ItemLoader
 
 class UnsplashComSpider(scrapy.Spider):
@@ -15,42 +15,33 @@ class UnsplashComSpider(scrapy.Spider):
 
 
     def parse(self, response):
+        """Функция, возвращающая ссылки всех результатов поиска.
+        По этим ссылкам паук в дальнейшем будет переходить и на новых страницах собирать информацию"""
         print(self.start_urls)
         links = response.xpath("//figure[@data-testid='photo-grid-masonry-figure']/link[@itemprop='acquireLicensePage']/@href").getall() # возвращает список ссылок всех атрибутов страницы по xpath
         print(links)
         for link in links:
+            # переходим по ссылке и на новой странице проводим сбор информации
             yield response.follow(link, callback=self.parse_img)
 
-    def correct_url(self, value):
-        return value.split(" ")[0]
+    def correct_url(self, value: str):
+        """Функция для обработки пакета ссылок картинок"""
+        print(value)
+        return [value.split(" ")[0]]
 
     def parse_img(self, response: HtmlResponse):
-        # commit = response.xpath('//p[@class="liDlw"]/text()').get()
-        # author = response.xpath('//a[@class="vGXaw uoMSP kXLw7 R6ToQ JVs7s R6ToQ"]/text()').get()
-        # loggin_of_author = response.xpath('//a[@class="vGXaw uoMSP kXLw7 R6ToQ JVs7s R6ToQ"]/@href').get()
-        # Published_datatime = response.xpath('//span[@class="IwfFI jhw7y"]/span/time/text()').get()
-        # url_img = response.xpath('//img[@class="I7OuT DVW3V L1BOa"]/@srcset').get()
-        # print(author, loggin_of_author)
-        # print(commit)
-        # print(Published_datatime)
-        # print(url_img)
-        # yield JobparserItem(author=author, commit=commit, Published_datatime=Published_datatime, url_img=url_img)
-
-        # формирование пакеда данных для выгрузки с сайта (loader это что то вроде обертки над items)
-        # Эта механика является альтернативой для  той что выше в коментах. Особеность ее в том что она разгружаешь pipeline за счет применения обработчиков в items
+        """Функция для формирование пакеда данных и выгрузки с сайта в items.py"""
         loader = ItemLoader(item=JobparserItem(), response=response)
-        # loader.add_xpath('commit', '//p[@class="liDlw"]/text()')
-        # loader.add_xpath('author', '//a[@class="vGXaw uoMSP kXLw7 R6ToQ JVs7s R6ToQ"]/text()')
-        # loader.add_xpath('loggin_of_author', '//a[@class="vGXaw uoMSP kXLw7 R6ToQ JVs7s R6ToQ"]/@href')
-        # loader.add_xpath('Published_datatime', '//span[@class="IwfFI jhw7y"]/span/time/text()')
+        loader.add_xpath('commit', '//p[@class="F7hSb"]/text()')
+        loader.add_xpath('author', '//a[@class="bimlc Pc_c1 rkYpC KZwZl wQd_A KZwZl"]/text()')
+        loader.add_xpath('loggin_of_author', '//a[@class="dYuCf"]/@href')
+        loader.add_xpath('Published_datatime', '//span[@class="X5fE_ yZhvJ"]/span/time/text()')
         # loader.add_xpath('image_urls', '//img[@class="I7OuT DVW3V L1BOa"]/@srcset')
 
-
-        image_urls = response.xpath('//img[@class="I7OuT DVW3V L1BOa"]/@srcset').get()
+        image_urls = response.xpath('//img[@class="tzC2N fbGdz cnmNG"]/@srcset').get()
         loader.add_value('image_urls', self.correct_url(image_urls))
 
         # передаем loader в pipelines
-        print("unsplash.com")
         yield loader.load_item()
 
 
